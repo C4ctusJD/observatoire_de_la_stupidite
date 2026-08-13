94e538ccf092e59280b4191f29c298d24434edc7
C4ctusJD <jeremdubus@gmail.com>
2026-08-13 18:39:37 +0200

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,12 @@
+export default function PageAccueil() {
+  return (
+    <main style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>L'Observatoire de la Stupidité</h1>
+      <p style={{ color: "#3B3B3B", marginTop: "1rem" }}>
+        Institution d'observation des comportements absurdes.
+        <br />
+        (Site en cours de construction.)
+      </p>
+    </main>
+  );
+}
